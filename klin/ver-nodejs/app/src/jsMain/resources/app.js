9f238d8e25cfde8be66f29646f30271962f21d7d
Михаил Капелько <kornerr@gmail.com>
2026-03-12 22:27:26 +0300

    before save

--- klin/ver-nodejs/app/src/jsMain/resources/app.js
+++ klin/ver-nodejs/app/src/jsMain/resources/app.js
@@ -16,7 +16,7 @@ function AppComponent() {
         // Effects
         let oneliners = [ 
             "inputFile", (c) => { appReadFile(c.inputFile) },
-            //"outputFileContents", (c) => { appWriteFile(c.outputFile, c.outputFileContents) },
+            "outputFileContents", (c) => { appWriteFile(c.outputFile, c.outputFileContents) },
         ];
         KT.registerOneliners(KT.appCtrl(), oneliners);
 
