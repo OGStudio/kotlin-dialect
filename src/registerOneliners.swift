fc61eb0b67c043ca0058d647f666c50108e0f228
Михаил Капелько <kornerr@gmail.com>
2026-03-13 22:03:00 +0300

    prepare src

---
+++ src/registerOneliners.swift
@@ -0,0 +1,18 @@
+// Bind effects to a Context changes
+func registerOneliners<T>(
+    _ ctrl: KDController,
+    _ items: [Any]
+) -> T? {
+    let halfCount = items.count / 2
+    for i in 0..<halfCount {
+        let field = items[i * 2] as! String
+        let callback = items[i * 2 + 1] as! (T) -> Void
+        ctrl.registerFieldCallback(fieldName: field) { cc in
+            let c = cc as! T
+            callback(c)
+        }
+    }
+
+    // A hack for generics to operate
+    return nil
+}
