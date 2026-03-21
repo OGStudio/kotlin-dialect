25ca3be875ec010eac35c1a2826c362614ccda43
Михаил Капелько <kornerr@gmail.com>
2026-03-21 21:41:23 +0300

    rm obs

--- example/ver-android/app/build.gradle.kts
+++ example/ver-android/app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -37,11 +36,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.coil.compose)
-    implementation(libs.coil.network)
-    implementation(libs.kotlinx.coroutines.android)
-    implementation(libs.kotlinx.serialization.json)
-    implementation(libs.okhttp)
 }
 
 kotlin {
